ce653c016fee9491d066bc69626294e18c458c5f
Jennifer Scroggins <jennifer@ravenwilde.com>
2025-05-09 22:18:15 -0400

    style: reorder tag filter and content sections in responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,9 +48,9 @@ export default async function Home({ searchParams }: { searchParams: { tag?: str
           {/* Tag filter */}
           {/* The TagFilter spans two columns on small screens (sm:col-span-2) */}
           {/* and one column on large screens (lg:col-span-1) to fit the responsive grid layout */}
-          <TagFilter tags={tags} className="sm:col-span-2 lg:col-span-1" />
+          <TagFilter tags={tags} className="sm:col-span-2 lg:col-span-1 lg:order-last" />
 
-          <div className="sm:col-span-2 lg:col-span-3">
+          <div className="sm:col-span-2 lg:col-span-3 lg:order-first">
             {/* Show featured post only when no tag filter is applied */}
             {showFeaturedPost && featuredPost && <FeaturedPost post={featuredPost} />}
 
